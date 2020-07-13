f14dfbeb171a51d634dc3452fc893492bf08361c
bbtrombone <bbtrombone@sbcglobal.net>
2020-07-13 16:21:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+let new_text = document.querySelector('h1#victory');
+new_text.innerHTML = "bob is the champion"
